6597314a7f022fbfdc000a2a9ae75bf8d702f4ce
Andreas Evensen <andreas.evensen11@gmail.com>
2020-06-13 12:22:55 +0200

    something

---
+++ Screen/Screen.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class Application(tk.Frame):
+    """ Makes a two-dimensional image """
+    
+    Width = 500
+    Height = 500
+    #Class variables that can be changed over time.
+
+    def __init__(self, master = None):
+        super().__init__(master)
+        self.master = master
+        self.drawmesh()
+
+    def drawmesh(self):
+        pass
