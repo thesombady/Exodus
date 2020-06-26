a5bcebffce6d2e1458307510305be96387398318
Andreas Evensen <andreas.evensen11@gmail.com>
2020-06-26 18:12:58 +0200

    commit

--- Screen/Screen.py
+++ Screen/Screen.py
@@ -1,1 +1,23 @@
 import tkinter as tk
+from PIL import Image, ImageTk
+
+class MainWindow(tk.Frame):
+    TITLE = "Exodus Game Engine"
+    def __init__(self, master = None, width = 1000, height = 800, GameTitle = None):
+        super().__init__(master)
+        self.master = master
+        self.width = width
+        self.height = height
+        self.master.geometry(f'{self.width}x{self.height}')
+        self.master.title(self.TITLE)
+        Canvas = tk.Canvas(self.master, width = 841, height = 422, bg = 'blue')
+        Canvas.pack()
+        img = ImageTk('Exodus.jpg')
+        Canvas.create_image(self.master, width = 841, height = 422, image=img)
+
+#851 × 422
+
+
+root = tk.Tk()
+app = MainWindow(root)
+app.mainloop()
