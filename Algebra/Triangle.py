23550eab3ac08ea080d6f01598dfb5586127e923
Andreas Evensen <andreas.evensen11@gmail.com>
2020-06-26 15:15:54 +0200

    commit

--- Algebra/Triangle.py
+++
@@ -1,8 +0,0 @@
-from Vector import vec2d
-from Matrix import Matrix2d
-
-class Triangle2d:
-    def __init__(self, vec1, vec2, vec3):
-        self.vec1 = vec1
-        self.vec2 = vec2
-        self.vec3 = vec3
--- Algebra/__init__.py
+++
@@ -1,4 +0,0 @@
-from Vector import vec2d
-from Matrix import Matrix2d
-from Triangle import Triangle
-from Mesh import mesh
